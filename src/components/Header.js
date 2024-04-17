f46de8ce6f0a855eff30f698f3c6fac2ce6d92f2
PesanduDU <pesandudubeysekara@gmail.com>
2024-04-17 11:22:26 +0530

    Third commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,40 @@
 import React from 'react'
 import './../componentCss/Header.css'
+import logo1 from './../assets/images/logo.jpg'
 
 export default function Header() {
   return (
     <header>
+      <div className="logo">
+        <a href="#"><img src={logo1} alt="" /></a>
+      </div>
       <nav>
-        <a href=''>Home</a>
-        <a href=''>About Us</a>
-        <a href=''>Photo Gallery</a>
-        <a href=''>Contact</a>
-        <a href=''>Login/Sign Up</a>
+        <ul>
+          <li><a href="#">Home</a></li>
+          {/* <li><a href="#">About Us</a></li> */}
+          <li><a href="#">About</a></li>
+          <li><a href="#">Gallery</a>
+            <ul className="submenu">
+              <li><a href="#">Dogs</a></li>
+              <li><a href="#">Cats</a></li>
+              <li><a href="#">Other</a></li>
+            </ul>
+          </li>
+          <li><a href="#">Store</a>
+          <ul className="submenu">
+              <li><a href="#">Dogs</a></li>
+              <li><a href="#">Cats</a></li>
+              <li><a href="#">Other</a></li>
+              <li><a href="#">Other</a></li>
+              <li><a href="#">Other</a></li>
+              <li><a href="#">Other</a></li>
+              <li><a href="#">Other</a></li>
+            </ul>
+          </li>
+          <li><a href="#">Review</a></li>
+          <li><a href="#">Contact</a></li>
+          <li><a href="#">Login</a></li>
+        </ul>
       </nav>
     </header>
   )
