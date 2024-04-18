4c3622ccc3ef603a98e0b613cf01d4e49f22c6af
PesanduDU <pesandudubeysekara@gmail.com>
2024-04-18 12:25:27 +0530

    adding pet details

---
+++ src/pages/OnePet.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function OnePet() {
+  return (
+    <div>OnePet</div>
+  )
+}
---
+++ src/pages/PetsDetails.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function PetsDetails() {
+  return (
+    petsArray = [
+        {
+            id: 1,
+            type: 'Dog',
+            name: 'Doggy',
+            age: 10,
+            photo: ''
+        }
+    ]
+  )
+}
