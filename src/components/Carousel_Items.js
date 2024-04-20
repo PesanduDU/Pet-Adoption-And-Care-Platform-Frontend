95029c84c8f277124ca771382f96d280c4bc8a3e
PesanduDU <pesandudubeysekara@gmail.com>
2024-04-20 21:57:50 +0530

    Update Home Page

--- src/components/Carousel_Items.js
+++ src/components/Carousel_Items.js
@@ -7,7 +7,7 @@ export default function Carousel_Items({img}) {
             <img src={img} alt="" />
             <div className="carousel-content">
                 <div className="carousel-author">Pet Care Platform</div>
-                <div className="carousel-title">Design Slider</div>
+                <div className="carousel-title">All Ways For You</div>
                 <div className="carousel-topic">Dogs and Cats</div>
                 <div className="carousel-des">
                     Our platform is a one-stop destination for pet lovers looking to adopt their newest furry friend and provide them with the utmost care. We connect prospective pet parents with shelters and rescues, making the adoption process seamless and joyful. Additionally, our site offers valuable resources, from expert tips on pet care to community forums where users can share experiences and seek advice. With our user-friendly interface and commitment to animal welfare, we're dedicated to creating loving homes for pets in need.
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './../componentCss/Header.css'
 import logo1 from './../assets/images/logo.jpg'
+import { FaUserCircle } from "react-icons/fa";
 
 export default function Header() {
   return (
@@ -21,19 +22,20 @@ export default function Header() {
             </ul>
           </li>
           <li><a href="#">Store</a>
-          <ul className="submenu">
-              <li><a href="#">Dogs</a></li>
-              <li><a href="#">Cats</a></li>
-              <li><a href="#">Other</a></li>
-              <li><a href="#">Other</a></li>
-              <li><a href="#">Other</a></li>
-              <li><a href="#">Other</a></li>
-              <li><a href="#">Other</a></li>
+            <ul className="submenu">
+              <li><a href="#">Pet Foods</a></li>
+              <li><a href="#">Pet </a></li>
             </ul>
           </li>
           <li><a href="#">Review</a></li>
           <li><a href="#">Contact</a></li>
-          <li><a href="#">Login</a></li>
+          <li><a href="#">Login</a>
+            <ul className="submenu">
+              <li><a href="#">User</a></li>
+              <li><a href="#">Admin</a></li>
+            </ul>
+          </li>
+          <li><FaUserCircle className='user-circle'/></li>
         </ul>
       </nav>
     </header>
