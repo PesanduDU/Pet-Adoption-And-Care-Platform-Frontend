79406265867918e1af6f3fb49da628d9320485be
PesanduDU <pesandudubeysekara@gmail.com>
2024-04-22 03:29:24 +0530

    update PetCard.tsx file
    Use google API

--- src/components/PetCard.tsx
+++ src/components/PetCard.tsx
@@ -1,22 +1,41 @@
+"use client";
 import React from 'react';
 import PetsDetails from '../data/PetsDetails.js';
 import './../componentCss/PetCard.css';
 import cat from './../assets/images/pets/meow.jpg'
-
-
 import { useEffect, useState } from 'react';
 import{
     APIProvider,
     Map,
+    AdvancedMarker,
     useMapsLibrary,
     useMap,
 } from '@vis.gl/react-google-maps'
 
+import {useJsApiLoader, GoogleMap, Marker} from '@react-google-maps/api'
+import { GiSkeleton } from 'react-icons/gi';
+
 export default function PetCard() {
-  const petsArray = PetsDetails()
+  // const petsArray = PetsDetails()
   const apiKeyAsString = String(process.env.REACT_APP_API_KEY);
+  const position = {lat:6.975579972848102, lng:79.91551612240723}
+  const position1 = {lat:8.27043731855174, lng:79.88354763525358}
+
+  const {isLoaded} = useJsApiLoader({
+    googleMapsApiKey:apiKeyAsString
+  })
+
+  if(!isLoaded){
+    return <h1>not loaded</h1>
+  }
 
   console.log(process.env)
+
+  // return (
+  //   <GoogleMap center={position1} zoom={13} mapContainerStyle={{width: '100vw',height:'100vh'}}>
+
+  //   </GoogleMap>
+  // )
 
   return (
     <div className='petcard'>
@@ -29,10 +48,35 @@ export default function PetCard() {
       </div>
 
       <div className="map">
-        <APIProvider apiKey={apiKeyAsString}>
+        <GoogleMap 
+          center={position} 
+          zoom={14} 
+          mapContainerStyle={{width: '540px',height:'540px'}}
+          options={{
+            zoomControl:false,
+            streetViewControl:false,
+            mapTypeControl:false,
+            fullscreenControl:false
+          }}
+        >
+          <Marker position={position} />
+        </GoogleMap>
+      </div>
 
-        </APIProvider>
+      <div className="location-topic">
+        <span>Pet Location</span>
       </div>
     </div>
   )
+
+  // return (
+  //   <APIProvider apiKey='AIzaSyCjzsoqGSNKp9IfKYsjJ2JY7CYAWmGswGQ'>
+  //     <div className='map-container'>
+  //       <Map 
+  //         zoom={9} 
+  //         center={position} >
+  //       </Map>
+  //     </div>
+  //   </APIProvider>
+  // )
 }
